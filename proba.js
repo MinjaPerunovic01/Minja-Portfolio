c0391302f4bc46cffcac82b3e5e686eb3e74ce6c
MinjaPerunovic <136376714+MinjaPerunovic01@users.noreply.github.com>
2023-06-24 16:55:27 +0200

    delete proba.js

--- proba.js
+++
@@ -1,8 +0,0 @@
-// $(window).scroll(function()
-// {
-//     $("#naslov1").css("top",Math.max(0,1000-$(this).scrollTop()));
-// });
-// var logoi = document.getElementById("naslov1");
-// if (logoi.style.top = "1000px") {
-//     logoi.style.opacity = "0";
-// } else (logoi.style.opacity = "1");
